cb491adfaf3916eadd28e69219a6f78fa4042348
Bilal Elmoussaoui <belmouss@redhat.com>
2026-02-01 23:57:01 +0100

    Bump MSRV and get rid of once_cell

--- pipewire/src/keys.rs
+++ pipewire/src/keys.rs
@@ -12,14 +12,13 @@
 //! ```
 
 use std::ffi::CStr;
-
-use once_cell::sync::Lazy;
+use std::sync::LazyLock;
 
 // unfortunately we have to take two args as concat_idents! is in experimental
 macro_rules! key_constant {
     ($name:ident, $pw_symbol:ident, #[doc = $doc:expr]) => {
         #[doc = $doc]
-        pub static $name: Lazy<&'static str> = Lazy::new(|| unsafe {
+        pub static $name: LazyLock<&'static str> = LazyLock::new(|| unsafe {
             CStr::from_bytes_with_nul_unchecked($crate::sys::$pw_symbol)
                 .to_str()
                 .unwrap()
